3d1912ab3785fc2f134df455463ff1a255268763
Petr "Stone" Hracek <phracek@redhat.com>
2021-03-03 10:19:37 +0100

    Git operation in bash script
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ jenkins_ci/update_github_pr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+CONTEXT=$1
+GIT_USER=$2
+GIT_PROJECT=$3
+
+pull_number=$(jq -r '.issue.number' <<< "$CI_MESSAGE")
+git fetch origin +refs/pull/*:refs/remotes/origin/pr/*
+git checkout origin/pr/$pull_number/head
+
+GIT_COMMIT=$(git rev-parse HEAD)
+
+python ${CI_SCRIPTS}/update_github_pr.py "$CONTEXT" "$GIT_USER" "$GIT_PROJECT" "$GIT_COMMIT"
