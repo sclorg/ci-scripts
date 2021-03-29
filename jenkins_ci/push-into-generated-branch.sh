641605448ff35bc95fa99056547cbb58554b10f8
Petr Hracek <phracek@redhat.com>
2021-03-29 11:22:20 +0200

    Update push-into-generated-branch.sh

--- jenkins_ci/push-into-generated-branch.sh
+++ jenkins_ci/push-into-generated-branch.sh
@@ -3,8 +3,6 @@
 # Script pushes changes into generated branch if exists
 set -ex
 
-GITHUB_PUSH_TOKEN=$1
-
 if git branch | grep -q generated; then
   GIT_URL=$(git config --get remote.origin.url)
   GIT_URL=${GIT_URL##git://}
