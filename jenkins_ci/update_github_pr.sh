3c49ba59b3e0cc9c4379891c0fbbfd9d5732c6f3
Petr "Stone" Hracek <phracek@redhat.com>
2021-04-06 11:25:41 +0200

    Check parameters and do not update PR on centos7.
    We have GitHub PR Builder on CentOS7.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- jenkins_ci/update_github_pr.sh
+++ jenkins_ci/update_github_pr.sh
@@ -6,6 +6,27 @@ CONTEXT=$1
 GIT_USER=$2
 GIT_PROJECT=$3
 
+if [ x"${CONTEXT}" == "x" ]; then
+  echo "The first parameter CONTEXT to upload_diff.sh has to be specified."
+  exit 1
+fi
+
+if [ x"${CONTEXT}" == "xcentos7" ]; then
+  echo "We do not use github2fedmsg in CentOS CI."
+  exit 0
+fi
+
+if [ x"${GITUSER}" == "x" ]; then
+  echo "The second parameter GITUSER to upload_diff.sh has to be specified."
+  exit 1
+fi
+
+if [ x"${GITPROJECT}" == "x" ]; then
+  echo "The third parameter GITPROJECT to upload_diff.sh has to be specified."
+  exit 1
+fi
+
+
 pull_number=$(jq -r '.issue.number' <<< "$CI_MESSAGE")
 git fetch origin +refs/pull/*:refs/remotes/origin/pr/*
 git checkout origin/pr/$pull_number/head
