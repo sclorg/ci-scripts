84aaac635734a411cb15a127eeac9018181e2db9
Petr "Stone" Hracek <phracek@redhat.com>
2022-05-09 10:13:23 +0200

    Update nightly builds for C9S and C8S
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- run_nightly_tests.sh
+++ run_nightly_tests.sh
@@ -3,21 +3,39 @@
 set -x
 
 LOGS_DIR="/home/fedora/logs"
-[[ -z "$1" ]] && { echo "You have to specify target to build SCL images. centos7, rhel7 or fedora" && exit 1 ; }
+[[ -z "$1" ]] && { echo "You have to specify target to build SCL images. c9s, c8s, rhel8, centos7, rhel7 or fedora" && exit 1 ; }
 TARGET="$1"
 shift
 [[ -z "$1" ]] && { echo "You have to specify type of the test to run. test, test-openshift, test-openshift-4" && exit 1 ; }
 TESTS="$1"
 
+TMT_REPO="https://gitlab.cee.redhat.com/platform-eng-core-services/sclorg-tmt-plans"
+TMT_BRANCH="master"
+API_RANCH="PRIVATE"
 if [[ "$TARGET" == "rhel8" ]]; then
   COMPOSE="RHEL-8-Updated"
 elif [[ "$TARGET" == "rhel7" ]]; then
   COMPOSE="RHEL-7-LatestUpdated"
 elif [[ "$TARGET" == "centos7" ]]; then
-  COMPOSE="CentOS-7-latest"
-  TESTING_TARGET="${TARGET}"
+  COMPOSE="CentOS-7"
+  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
+  TMT_BRANCH="main"
+  API_RANCH="PUBLIC"
 elif [[ "$TARGET" == "fedora" ]]; then
-  COMPOSE="CentOS-7-latest"
+  COMPOSE="Fedora-latest"
+  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
+  TMT_BRANCH="main"
+  API_RANCH="PUBLIC"
+elif [[ "$TARGET" == "c9s" ]]; then
+  COMPOSE="CentOS-Stream-9"
+  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
+  TMT_BRANCH="main"
+  API_RANCH="PUBLIC"
+elif [[ "$TARGET" == "c8s" ]]; then
+  COMPOSE="CentOS-Stream-8"
+  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
+  TMT_BRANCH="main"
+  API_RANCH="PUBLIC"
 else
   echo "This target is not supported"
   exit 1
@@ -31,26 +49,27 @@ fi
 JOB_JSON="job-${TARGET}-${TESTS}.json"
 REQUEST_JSON="request-${TARGET}-${TESTS}.json"
 RESPONSE_JSON="response-${TARGET}-${TESTS}.json"
-cd /home/fedora
+cd /home/fedora || { echo "Could not switch to /home/fedora"; exit 1; }
 if [[ ! -d "${LOGS_DIR}" ]]; then
   mkdir -p "${LOGS_DIR}"
 fi
 
 LOG="${LOGS_DIR}/$TARGET-$TESTS.log"
-date > ${LOG}
+date > "${LOG}"
 curl -L https://url.corp.redhat.com/fmf-data > /tmp/fmf_data
 source /tmp/fmf_data
+API_KEY="${API_KEY}_${API_RANCH}"
 
-echo "TARGET is: ${TARGET} and test is: ${TESTS}" | tee -a ${LOG}
+echo "TARGET is: ${TARGET} and test is: ${TESTS}" | tee -a "${LOG}"
 
 function final_report() {
-  echo "FINAL REPORT for ${REQ_ID}" | tee -a ${LOG}
-  curl $TF_ENDPOINT/requests/$REQ_ID > "${JOB_JSON}"
-  cat "${JOB_JSON}" | tee -a ${LOG}
+  echo "FINAL REPORT for ${REQ_ID}" | tee -a "${LOG}"
+  curl "$TF_ENDPOINT/requests/$REQ_ID" > "${JOB_JSON}"
+  cat "${JOB_JSON}" | tee -a "${LOG}"
   state=$(jq -r .state "${JOB_JSON}")
   result=$(jq -r .result.overall "${JOB_JSON}")
-  echo "STATE: $state" | tee -a ${LOG}
-  echo "RESULT: $result" | tee -a ${LOG}
+  echo "STATE: $state" | tee -a "${LOG}"
+  echo "RESULT: $result" | tee -a "${LOG}"
   new_state="success"
   infra_error=" "
   echo "State is $state and result is: $result"
@@ -63,27 +82,23 @@ function final_report() {
     infra_error=" - Infra problems"
     new_state="failure"
   fi
-  if [[ x"$new_state" == "failure" ]]; then
-    curl $TF_LOG/$REQ_ID/pipeline.log > "${RESULT_DIR}/${repo}.log"
+  if [[ x"$new_state" == x"failure" ]]; then
+    curl "$TF_LOG/$REQ_ID/pipeline.log" > "${RESULT_DIR}/${TARGET}.log"
   fi
-  echo "New State: $new_state" | tee -a ${LOG}
-  echo "Infra state: $infra_error" | tee -a ${LOG}
+  echo "New State: $new_state" | tee -a "${LOG}"
+  echo "Infra state: $infra_error" | tee -a "${LOG}"
 }
 
 
 function schedule_testing_farm_request() {
-  echo "Schedule job for: $TARGET" | tee -a ${LOG}
-  TESTING_PLAN="${TARGET}"
-  if [[ "$TARGET" == "fedora" ]]; then
-    TESTING_PLAN="centos7"
-  fi
-  cat << EOF > ${REQUEST_JSON}
+  echo "Schedule job for: $TARGET" | tee -a "${LOG}"
+  cat << EOF > "${REQUEST_JSON}"
     {
       "api_key": "$API_KEY",
       "test": {"fmf": {
-      "url": "https://gitlab.cee.redhat.com/platform-eng-core-services/sclorg-tmt-plans",
-      "ref": "master",
-      "name": "nightly-container-${TESTING_PLAN}"
+      "url": "${TMT_REPO}",
+      "ref": "${TMT_BRANCH}",
+      "name": "nightly-container-${TARGET}"
       }},
       "environments": [{
       "arch": "x86_64",
@@ -94,29 +109,29 @@ function schedule_testing_farm_request() {
       }}]
     }
 EOF
-  cat "${REQUEST_JSON}" | tee -a ${LOG}
-  curl $TF_ENDPOINT/requests --data @${REQUEST_JSON} --header "Content-Type: application/json" --output ${RESPONSE_JSON}
-  cat "${RESPONSE_JSON}" | tee -a ${LOG}
+  cat "${REQUEST_JSON}" | tee -a "${LOG}"
+  curl "$TF_ENDPOINT/requests" --data @${REQUEST_JSON} --header "Content-Type: application/json" --output "${RESPONSE_JSON}"
+  cat "${RESPONSE_JSON}" | tee -a "${LOG}"
   REQ_ID=$(jq -r .id "${RESPONSE_JSON}")
-  echo "$REQ_ID" | tee -a ${LOG}
+  echo "$REQ_ID" | tee -a "${LOG}"
 }
 
 function check_testing_farm_status() {
-  echo "Check state for $REQ_ID" | tee -a ${LOG}
+  echo "Check state for $REQ_ID" | tee -a "${LOG}"
   CMD="$TF_ENDPOINT/requests/$REQ_ID"
-  echo "Command for checking state is: ${CMD}" | tee -a ${LOG}
+  echo "Command for checking state is: ${CMD}" | tee -a "${LOG}"
   curl $CMD > "${JOB_JSON}"
   state=$(jq -r .state "${JOB_JSON}")
   # Wait till job is not finished. As soon as state is complete or failure then go to the finish action
   while [ "$state" == "running" ] || [ "$state" == "new" ] || [ "$state" == "pending" ] || [ "$state" == "queued" ]; do
     # Wait 300s. We do not need to query Testing Farm each second
     sleep 300
-    date | tee -a ${LOG}
+    date | tee -a "${LOG}"
     echo "${CMD}" | tee -a "${LOG}"
-    curl $CMD > "${JOB_JSON}"
-    cat "${JOB_JSON}" | tee -a ${LOG}
+    curl "$CMD" > "${JOB_JSON}"
+    cat "${JOB_JSON}" | tee -a "${LOG}"
     state=$(jq -r .state "${JOB_JSON}")
-    echo "$state" | tee -a ${LOG}
+    echo "$state" | tee -a "${LOG}"
   done
 }
 
