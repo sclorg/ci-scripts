68ffef80365a777e034df1f3cc2a3ba08e6e550b
Petr "Stone" Hracek <phracek@redhat.com>
2022-05-25 08:45:59 +0200

    Use log per each OS and save also rhcwt output to GRADES.log
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- daily_tests/daily_grades.sh
+++ daily_tests/daily_grades.sh
@@ -6,10 +6,10 @@ OS=$1
 [[ -z $OS ]] && { echo "OS for checking grades was not specified." && echo "Command is: $0 <RHEL7|RHEL8>" && exit 1 ; }
 
 LOGS_DIR="/home/fedora/logs"
-SCRIPT_LOG="$LOGS_DIR/grades.log"
+SCRIPT_LOG="$LOGS_DIR/grades-$OS.log"
 GRADE_FLAG=0
 if [[ "$OS" == "RHEL7" ]]; then
-  GRADES_LOG="$HOME/logs/rhel8-grades"
+  GRADES_LOG="$HOME/logs/rhel7-grades"
   RHCWT_CONFIG="rhel7.yaml"
 
 elif [[ "$OS" == "RHEL8" ]]; then
@@ -20,11 +20,10 @@ fi
 date | tee $SCRIPT_LOG
 
 echo "Configuration file is $RHCWT_CONFIG and logs are in $GRADES_LOG" | tee -a "${SCRIPT_LOG}"
-SUMMARY="Grades for"
+SUMMARY="[CS Image Grading] Grades for"
 function check_grades() {
   echo "GRADE_FLAG: $GRADE_FLAG" | tee -a "${SCRIPT_LOG}"
   while read -r line; do
-    echo "X $line X"
     if [[ ! "$line" == *"[A]"* ]]; then
       GRADE_FLAG=1
       echo "GRADE_FLAG: $GRADE_FLAG" | tee -a "${SCRIPT_LOG}"
@@ -34,7 +33,11 @@ function check_grades() {
 }
 
 function get_grades() {
-  rhcwt --base WHATEVER --config "$RHCWT_CONFIG" rhcc grades > "$GRADES_LOG"
+  local exclude_image=""
+  if [[ "$OS" == "RHEL8" ]]; then
+    exclude_image="--exclude-image nodejs-10"
+  fi
+  /home/fedora/.local/bin/rhcwt --base WHATEVER $exclude_image --config "$RHCWT_CONFIG" rhcc grades > "$GRADES_LOG" 2>&1
   tee -a "${SCRIPT_LOG}" < "${GRADES_LOG}"
   return 0
 }
