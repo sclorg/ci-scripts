235ae27c4800757736336afae3e237fb9701f82b
Adam Dobes <adobes@redhat.com>
2023-08-24 13:36:56 +0200

    Fix code block escaping and error handling

--- jenkins_ci/update_quay_description.py
+++ jenkins_ci/update_quay_description.py
@@ -9,16 +9,6 @@ import requests
 from typing import Optional, List
 
 
-token = os.environ["REGISTRY_TOKEN"] # Quay token
-image_name = os.environ["IMAGE_NAME"] # Quay image name
-registry_namespace = os.environ["REGISTRY_NAMESPACE"] # Quay namespace
-context = os.environ["DOCKER_CONTEXT"] # Build Context
-github_repo = os.environ["GITHUB_REPO"] # Name of repo on github
-suffix = ""
-if "SUFFIX" in os.environ:
-    suffix = f"-{os.environ['SUFFIX']}"
-
-
 def print_api_error(status_code: int) -> None:
     errors = {400: "Bad Request",
               401: "Session required",
@@ -32,6 +22,9 @@ def print_api_error(status_code: int) -> None:
 
 def load_readme_as_list(dir: str) -> Optional[List[str]]:
     readme_path = os.path.join(dir, "README.md")
+    if not os.path.isfile(readme_path):
+        print(f"Invalid path: {readme_path} does not exist")
+        return None
     with open(readme_path) as readme:
         lines = readme.readlines()
         for i, line in enumerate(lines):
@@ -39,11 +32,22 @@ def load_readme_as_list(dir: str) -> Optional[List[str]]:
                 if i + 3 >= len(lines):
                     break
                 return lines[i + 3:]
+    print("Invalid README format")
     return None
 
 
+def escape_code_block(lines: List[str]):
+    backtick_count = 0
+    for line in lines:
+        if "```" in line:
+            backtick_count += 1
+    if backtick_count % 2 == 1:
+        lines.append("...\n```")
+    return lines
+
+
 def update_description(readme: str) -> int:
-    api_request_path = f"https://quay.io/api/v1/repository/{registry_namespace}/{image_name}{suffix}"
+    api_request_path = f"https://quay.io/api/v1/repository/{registry_namespace}/{image_name}"
     headers = {"Content-Type": "application/json",
                "Authorization": f"Bearer {token}"}
     data = {"description": readme}
@@ -52,18 +56,20 @@ def update_description(readme: str) -> int:
 
 
 if __name__ == "__main__":
-    if registry_namespace != "sclorg" and registry_namespace != "centos7":
-        print("Invalid namespace. This script supports only sclorg and centos7 namespaces")
-        sys.exit(1)
-
+    token = os.environ["REGISTRY_TOKEN"] # Quay token
+    image_name = os.environ["IMAGE_NAME"] # Quay image name
+    registry_namespace = os.environ["REGISTRY_NAMESPACE"] # Quay namespace
+    context = os.environ["DOCKER_CONTEXT"] # Build Context
+    github_repo = os.environ["GITHUB_REPO"] # Name of repo on github
+    
     readme_as_list = load_readme_as_list(context)
     if readme_as_list is None:
-        print("Invalid README format")
         sys.exit(1)
     if len(readme_as_list) > 100:
         readme_as_list = readme_as_list[:98]
+        escape_code_block(readme_as_list)
         readme_as_list.append("\n<br>\n")
-        readme_as_list.append(f"Learn more at <https://github.com/sclorg/{github_repo}/{context}/README.md>")
+        readme_as_list.append(f"Learn more at <https://github.com/sclorg/{github_repo}/blob//master/{context}/README.md>")
     
     readme = "".join(readme_as_list)
     request_status = update_description(readme)
