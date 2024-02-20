7869e74a622127cf4124bccdfb15e5e8afbb2099
Zuzana Miklankova <zmiklank@redhat.com>
2024-02-20 09:40:29 +0100

    move testing jsons to data file

--- ocp-stream-generator/tests/test_yaml_loader.py
+++ ocp-stream-generator/tests/test_yaml_loader.py
@@ -4,8 +4,8 @@ import pytest
 import sys
 
 from stream_generator import YamlLoader
+from data.data_yaml_loader import load_yaml_result
 
 def test_load_yaml():
     loader = YamlLoader("tests/data/minimal.yml")
-    result = {"name": "test_pkg", "pretty_name": "Test Package", "sample_repo": "", "category": "testing", "description": "Let's test this!", "imagestream_files": [{"filename": "test-centos.json", "latest": "2-el9", "distros": [{"name": "CentOS Stream 8", "app_versions": [2, 3, 1]}, {"name": "CentOS Stream 9", "app_versions": [2]}]}]}
-    assert loader.json_data == result
+    assert loader.json_data == load_yaml_result
