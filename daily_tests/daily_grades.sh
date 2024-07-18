36f484ac29003d12bec481bea8a2604a8b6d1141
Petr "Stone" Hracek <phracek@redhat.com>
2024-07-18 10:07:56 +0200

    Get rid off Jenkins, delete rhel7 and update composes
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- daily_tests/daily_grades.sh
+++ daily_tests/daily_grades.sh
@@ -3,17 +3,13 @@
 set -x
 
 OS=$1
-[[ -z $OS ]] && { echo "OS for checking grades was not specified." && echo "Command is: $0 <RHEL7|RHEL8|RHEL9>" && exit 1 ; }
+[[ -z $OS ]] && { echo "OS for checking grades was not specified." && echo "Command is: $0 <RHEL8|RHEL9>" && exit 1 ; }
 
 LOGS_DIR="/home/fedora/logs"
 SCRIPT_LOG="$LOGS_DIR/grades-$OS.log"
 GRADE_FLAG=0
 GRADE_NONE=0
-if [[ "$OS" == "RHEL7" ]]; then
-  GRADES_LOG="$HOME/logs/rhel7-grades"
-  RHCWT_CONFIG="rhel7.yaml"
-
-elif [[ "$OS" == "RHEL8" ]]; then
+if [[ "$OS" == "RHEL8" ]]; then
   GRADES_LOG="$HOME/logs/rhel8-grades"
   RHCWT_CONFIG="rhel8.yaml"
 
