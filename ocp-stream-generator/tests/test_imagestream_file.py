408b2aa189f2cc3903d34b0fba33b608d5a586df
Petr "Stone" Hracek <phracek@redhat.com>
2024-08-19 10:31:47 +0200

    Fix python issues caught by pre-commit-check
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- ocp-stream-generator/tests/test_imagestream_file.py
+++ ocp-stream-generator/tests/test_imagestream_file.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-import pytest
-import sys
-import re
+
 from stream_generator import ImagestreamFile
 
 
-
 def test_create_isf():
-    isf = {"filename": "test-centos.json", "latest": "2-el9", "distros": [{"name": "CentOS Stream 8", "app_versions": [2, 3, 1]}, {"name": "CentOS Stream 9", "app_versions": [2]}]}
-    isf_header = {"name": "test_pkg", "pretty_name": "Test Package", "sample_repo": "", "category": "testing", "description": "Let's test this!"}
+    isf = {
+        "filename": "test-centos.json",
+        "latest": "2-el9",
+        "distros": [
+            {"name": "CentOS Stream 8", "app_versions": [2, 3, 1]},
+            {"name": "CentOS Stream 9", "app_versions": [2]},
+        ],
+    }
+    isf_header = {
+        "name": "test_pkg",
+        "pretty_name": "Test Package",
+        "sample_repo": "",
+        "category": "testing",
+        "description": "Let's test this!",
+    }
     isf_data = ImagestreamFile(isf, isf_header)
     assert isf_data.latest_tag.stream_name == "latest"
     assert isf_data.latest_tag.image == "2-el9"
@@ -18,9 +28,26 @@ def test_create_isf():
     assert isf_data.latest_tag.distro_name == "CentOS Stream 9"
     assert isf_data.tags[0].description == "Let's test this!"
 
+
 def test_create_isf_custom_tags():
-    isf = {"filename": "test-centos.json", "custom_tags": [{"name": "my custom tag", "app_version": "11", "distro": "UBI 8"}], "latest": "2-el9", "distros": [{"name": "CentOS Stream 8", "app_versions": [2, 3, 1]}, {"name": "CentOS Stream 9", "app_versions": [2]}]}
-    isf_header = {"name": "test_pkg", "pretty_name": "Test Package", "sample_repo": "", "category": "testing", "description": "Let's test this!"}
+    isf = {
+        "filename": "test-centos.json",
+        "custom_tags": [
+            {"name": "my custom tag", "app_version": "11", "distro": "UBI 8"}
+        ],
+        "latest": "2-el9",
+        "distros": [
+            {"name": "CentOS Stream 8", "app_versions": [2, 3, 1]},
+            {"name": "CentOS Stream 9", "app_versions": [2]},
+        ],
+    }
+    isf_header = {
+        "name": "test_pkg",
+        "pretty_name": "Test Package",
+        "sample_repo": "",
+        "category": "testing",
+        "description": "Let's test this!",
+    }
     isf_data = ImagestreamFile(isf, isf_header)
     print(isf_data.tags)
     assert isf_data.latest_tag.stream_name == "latest"
--- ocp-stream-generator/tests/test_json_builder.py
+++ ocp-stream-generator/tests/test_json_builder.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import pytest
-import sys
 import re
 from stream_generator import JsonBuilder
 from stream_generator import Tag
@@ -15,46 +13,78 @@ from data.data_json_builder import create_annotation_result
 from data.data_json_builder import create_annotation_latest_result
 from data.data_json_builder import create_header_result
 from data.data_json_builder import generate_json_result
+
 builder = JsonBuilder()
 
-header = {"name": "test", "pretty_name": "Test", "sample_repo": "", "category": "test", "description": "test description"}
-
+header = {
+    "name": "test",
+    "pretty_name": "Test",
+    "sample_repo": "",
+    "category": "test",
+    "description": "test description",
+}
 
 
 def test_add_tag():
     tag = Tag(header, "RHEL 8", "private", 1)
-    assert builder.add_tag({"spec": { "tags": []}}, tag)  ==  add_tag_result
+    assert builder.add_tag({"spec": {"tags": []}}, tag) == add_tag_result
+
 
 def test_add_latest_tag():
     tag_latest = LatestTag(header, "RHEL 8", "private", "1-el8")
-    assert builder.add_tag({"spec": { "tags": []}}, tag_latest)  ==  add_tag_latest_result
+    assert builder.add_tag({"spec": {"tags": []}}, tag_latest) == add_tag_latest_result
+
 
 def test_add_custom_tag():
-    tag_custom = CustomTag(header, "private", {"distro": "RHEL 8", "name": "some custom name", "app_version": "13"})
-    assert builder.add_tag({"spec": { "tags": []}}, tag_custom)  ==  add_tag_custom_result
+    tag_custom = CustomTag(
+        header,
+        "private",
+        {"distro": "RHEL 8", "name": "some custom name", "app_version": "13"},
+    )
+    assert builder.add_tag({"spec": {"tags": []}}, tag_custom) == add_tag_custom_result
+
 
 def test_create_annotation():
     tag = Tag(header, "RHEL 8", "private", 1)
     assert builder.create_annotation(tag) == create_annotation_result
 
+
 def test_create_annotation_latest():
     latest_tag = LatestTag(header, "RHEL 8", "private", "1-el8")
     assert builder.create_annotation(latest_tag) == create_annotation_latest_result
 
+
 def test_create_header():
-    header = {"name": "test", "pretty_name": "Test", "category": "test", \
-              "sample_repo": "", "description": "test description"}
-    file = {"filename": "test-file.json", "distros": \
-           [{"name": "RHEL 8", "app_versions": [1]}], "latest": "1-el8"}
+    header = {
+        "name": "test",
+        "pretty_name": "Test",
+        "category": "test",
+        "sample_repo": "",
+        "description": "test description",
+    }
+    file = {
+        "filename": "test-file.json",
+        "distros": [{"name": "RHEL 8", "app_versions": [1]}],
+        "latest": "1-el8",
+    }
     isf_data = ImagestreamFile(file, header)
     assert builder.create_header(isf_data) == create_header_result
 
+
 def test_generate_json():
-    header = {"name": "test", "pretty_name": "Test", "category": "test", \
-              "sample_repo": "", "description": "test description"}
-    file = {"filename": "test-file.json", "distros": \
-           [{"name": "RHEL 8", "app_versions": [1]}], "latest": "1-el8"}
+    header = {
+        "name": "test",
+        "pretty_name": "Test",
+        "category": "test",
+        "sample_repo": "",
+        "description": "test description",
+    }
+    file = {
+        "filename": "test-file.json",
+        "distros": [{"name": "RHEL 8", "app_versions": [1]}],
+        "latest": "1-el8",
+    }
     isf_data = ImagestreamFile(file, header)
-    stripped_json= re.sub(r"[\n\t\s\\n]*", "", builder.generate_json(isf_data))
-    stripped_result=re.sub(r"[\n\t\s\\n]*", "", generate_json_result)
+    stripped_json = re.sub(r"[\n\t\s\\n]*", "", builder.generate_json(isf_data))
+    stripped_result = re.sub(r"[\n\t\s\\n]*", "", generate_json_result)
     assert stripped_json == stripped_result
