cd18ee32e3eba0fa6b98471278dc0117b0ab304a
Petr "Stone" Hracek <phracek@redhat.com>
2024-10-07 14:49:14 +0200

    Copy logs always
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- run_nightly_helm_charts.sh
+++ run_nightly_helm_charts.sh
@@ -47,13 +47,13 @@ $TMT_COMMAND | tee -a "${LOG}"
 if [[ $? -ne 0 ]]; then
   echo "TMT command $TMT_COMMAND has failed."
   touch "${RESULTS_DIR}/${TARGET}-${TESTS}/tmt_failed"
-  if [[ -d "${DIR}/plans/${TFT_PLAN}/data/results" ]]; then
-    cp -rv "${DIR}/plans/${TFT_PLAN}/data/results/*" "${RESULTS_DIR}/${TARGET}-${TESTS}/plans/${TFT_PLAN}/data/results/"
-  fi
-  cp "${DIR}/log.txt" "${RESULTS_DIR}/${TARGET}-${TESTS}/"
 else
   touch "${RESULTS_DIR}/${TARGET}-${TESTS}/tmt_success"
 fi
+if [[ -d "${DIR}/plans/${TFT_PLAN}/data/results" ]]; then
+  cp -rv "${DIR}/plans/${TFT_PLAN}/data/results/*" "${RESULTS_DIR}/${TARGET}-${TESTS}/plans/${TFT_PLAN}/data/results/"
+fi
+cp "${DIR}/log.txt" "${RESULTS_DIR}/${TARGET}-${TESTS}/"
 set +o pipefail
 rm -f "${RESULTS_DIR}/${TARGET}-${TESTS}/tmt_running"
 cd "$CWD" || exit 1
