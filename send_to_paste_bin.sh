465726bf9d15314f71e627f6bade2b37597f34ff
Petr Hracek <phracek@redhat.com>
2025-04-02 15:20:16 +0200

    Extend sending logs from 1week->1month

--- send_to_paste_bin.sh
+++ send_to_paste_bin.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-/usr/local/bin/pbincli send --server https://privatebin.corp.redhat.com --expire 1week --no-insecure-warning --no-check-certificate --format plaintext < $1 > $2 2>&1
+/usr/local/bin/pbincli send --server https://privatebin.corp.redhat.com --expire 1month --no-insecure-warning --no-check-certificate --format plaintext < $1 > $2 2>&1
