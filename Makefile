6752d9e86a8509e09e49632a9f665339316a42a9
Petr "Stone" Hracek <phracek@redhat.com>
2026-01-20 09:26:46 +0100

    First shot with OpenShift templates for NightlyBuilds
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-.PHONY: run_daily_tests shellcheck
+.PHONY: run_daily_tests shellcheck build_images
 
 run_daily_tests:
 	bash daily_tests/daily_scl_tests.sh
 
 shellcheck:
 	./run-shellcheck.sh `git ls-files *.sh`
+
+build_images:
+	podman build -t quay.io/sclorg/daily-tests:latest -f Containerfile.daily-tests .
+	podman build -t quay.io/sclorg/daily-reports:latest -f Containerfile.daily-reports .
--- run_nightly_tests.sh
+++ run_nightly_tests.sh
@@ -1,117 +1,138 @@
 #!/bin/bash
 
-set -x
-
-LOGS_DIR="$HOME/logs"
-[[ -z "$1" ]] && { echo "You have to specify target to build SCL images. rhel9, rhel8, or fedora" && exit 1 ; }
-TARGET="$1"
-shift
-[[ -z "$1" ]] && { echo "You have to specify type of the test to run. test, test-pytest, test-openshift, test-openshift-pytest, test-openshift-4" && exit 1 ; }
-TESTS="$1"
-shift
+[[ -z "$TARGET" ]] && { echo "You have to specify target to build SCL images. rhel9, rhel8, or fedora" && exit 1 ; }
+[[ -z "$TESTS" ]] && { echo "You have to specify type of the test to run. test, test-pytest, test-openshift, test-openshift-pytest" && exit 1 ; }
 SET_TEST=""
 if [[ "${TESTS}" != "test-upstream" ]]; then
-  [[ -z "$1" ]] && { echo "You have to specify type of images S2I or NOS2I" && exit 1 ; }
-  SET_TEST="$1"
+  [[ -z "$TEST_TYPE" ]] && { echo "You have to specify type of images S2I or NOS2I" && exit 1 ; }
+  SET_TEST="$TEST_TYPE"
 fi
 
-TMT_REPO="https://gitlab.cee.redhat.com/platform-eng-core-services/sclorg-tmt-plans"
+LOGS_DIR="/var/tmp/daily_tests_logs/"
+LOGS_DIR_OLD="${LOGS_DIR}/old"
 DAILY_TEST_DIR="/var/tmp/daily_scl_tests"
 RESULTS_DIR="/var/tmp/daily_reports_dir"
+RESULTS_DIR_OLD="${RESULTS_DIR}/old"
+SCRIPT="daily_scl_tests"
 TMT_DIR="sclorg-tmt-plans"
-API_KEY="API_KEY_PRIVATE"
 TFT_PLAN="nightly-container-$TARGET"
-if [[ "$TARGET" == "rhel8" ]]; then
-  COMPOSE="1MT-RHEL-8.10.0-updates"
-elif [[ "$TARGET" == "rhel9" ]]; then
-  COMPOSE="1MT-RHEL-9.6.0-updates"
-elif [[ "$TARGET" == "rhel10" ]]; then
-  COMPOSE="1MT-RHEL-10.0"
-elif [[ "$TARGET" == "fedora" ]]; then
-  COMPOSE="1MT-Fedora-40"
-  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
-  TMT_DIR="sclorg-testing-farm"
-  TFT_PLAN="nightly-container-f"
-elif [[ "$TARGET" == "c9s" ]]; then
-  COMPOSE="1MT-CentOS-Stream-9"
-  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
-  TMT_DIR="sclorg-testing-farm"
-  TFT_PLAN="nightly-container-centos-stream-9"
-elif [[ "$TARGET" == "c10s" ]]; then
-  COMPOSE="1MT-CentOS-Stream-10"
-  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
-  TMT_DIR="sclorg-testing-farm"
-  TFT_PLAN="nightly-container-centos-stream-10"
-elif [[ "$TARGET" == "c10s" ]]; then
-  COMPOSE="1MT-CentOS-Stream-10"
-  TMT_REPO="https://github.com/sclorg/sclorg-testing-farm"
-  TMT_DIR="sclorg-testing-farm"
-  TFT_PLAN="nightly-container-centos-stream-10"
-else
-  echo "This target is not supported"
-  exit 1
+DIR="${DAILY_TEST_DIR}/${TARGET}-${TESTS}-${SET_TEST}"
+RESULTS_TARGET_DIR="${RESULTS_DIR}/${TARGET}-${TESTS}"
+if [[ "$TESTS" == "test-upstream" ]]; then
+  DIR="${DAILY_TEST_DIR}/${TARGET}-${TESTS}"
 fi
+LOG_FILE="${LOGS_DIR}/${TARGET}-${TESTS}.log"
+WORK_DIR=$(mktemp -d -p "/var/tmp")
+
+function move_logs_to_old() {
+  rm -rf "${LOGS_DIR_OLD}/*"
+  rm -rf "${RESULTS_DIR_OLD}/*"
+  mv "${LOG_FILE}" "${LOGS_DIR_OLD}/"
+  mv "${RESULTS_TARGET_DIR}" "${RESULTS_DIR_OLD}/"
+}
+
+function prepare_environment() {
+  if [[ ! -d "${LOGS_DIR}" ]]; then
+    mkdir -p "${LOGS_DIR}"
+  fi
+  if [[ ! -d "${RESULTS_DIR}" ]]; then
+    mkdir -p "${RESULTS_DIR}"
+  fi
+  if [[ ! -d "${LOGS_DIR_OLD}" ]]; then
+    mkdir -p "${LOGS_DIR_OLD}"
+  fi
+  if [[ ! -d "${RESULTS_DIR_OLD}" ]]; then
+    mkdir -p "${RESULTS_DIR_OLD}"
+  fi
+  mkdir -p "${RESULTS_TARGET_DIR}/plans/${TFT_PLAN}/data/results"
+  mkdir -p "$DIR"
+}
+
+function get_compose() {
+  if [[ "$TARGET" == "rhel8" ]]; then
+    COMPOSE="1MT-RHEL-8.10.0-updates"
+    TMT_DIR="$DOWNSTREAM_TMT_DIR"
+  elif [[ "$TARGET" == "rhel9" ]]; then
+    COMPOSE="1MT-RHEL-9.6.0-updates"
+    TMT_DIR="$DOWNSTREAM_TMT_DIR"
+  elif [[ "$TARGET" == "rhel10" ]]; then
+    COMPOSE="1MT-RHEL-10.0"
+    TMT_DIR="$DOWNSTREAM_TMT_DIR"
+  elif [[ "$TARGET" == "fedora" ]]; then
+    COMPOSE="1MT-Fedora-40"
+    TMT_DIR="$UPSTREAM_TMT_DIR"
+  elif [[ "$TARGET" == "c9s" ]]; then
+    COMPOSE="1MT-CentOS-Stream-9"
+    TMT_DIR="$UPSTREAM_TMT_DIR"
+  elif [[ "$TARGET" == "c10s" ]]; then
+    COMPOSE="1MT-CentOS-Stream-10"
+    TMT_DIR="$UPSTREAM_TMT_DIR"
+  else
+    echo "This target is not supported"
+    exit 1
+  fi
+  COMPOSE=$(tmt -q run provision -h minute --list-images | grep $COMPOSE | head -n 1 | tr -d '[:space:]')
+  export COMPOSE
+}
+
+function run_tests() {
+  ENV_VARIABLES="-e DEBUG=yes -e SCRIPT=$SCRIPT -e OS=$TARGET"
+  if [[ "$TESTS" == "test-upstream" ]]; then
+    ENV_VARIABLES="$ENV_VARIABLES -e TEST=$TESTS"
+  else
+    ENV_VARIABLES="$ENV_VARIABLES -e SET_TEST=$SET_TEST"
+  fi
+  TMT_COMMAND="tmt run -v -v -d -d --all ${ENV_VARIABLES} --id ${DIR} plan --name $TFT_PLAN provision --how minute --auto-select-network --image ${COMPOSE}"
+  echo "TMT command is: $TMT_COMMAND" | tee -a "${LOG}"
+  touch "${RESULTS_TARGET_DIR}/tmt_running"
+  set -o pipefail
+  $TMT_COMMAND | tee -a "${LOG}"
+  if [[ $? -ne 0 ]]; then
+    echo "TMT command $TMT_COMMAND has failed."
+    if [[ -f "${RESULTS_TARGET_DIR}/tmt_success" ]]; then
+      rm -f "${RESULTS_TARGET_DIR}/tmt_success"
+    fi
+    touch "${RESULTS_TARGET_DIR}/tmt_failed"
+  else
+    if [[ -f "${RESULTS_TARGET_DIR}/tmt_failed" ]]; then
+      echo "Previous test run has failed but this one has passed."
+    else
+      touch "${RESULTS_TARGET_DIR}/tmt_success"
+    fi
+  fi
+  if [[ -d "${DIR}/plans/${TFT_PLAN}/data" ]]; then
+    cp -rv "${DIR}/plans/${TFT_PLAN}/data/results" "${RESULTS_TARGET_DIR}/plans/${TFT_PLAN}/data/"
+    cp -v "${DIR}/plans/${TFT_PLAN}/data/*.log" "${RESULTS_TARGET_DIR}/plans/${TFT_PLAN}/data/"
+  fi
+  cp "${DIR}/log.txt" "${RESULTS_TARGET_DIR}/"
+  set +o pipefail
+  rm -f "${RESULTS_TARGET_DIR}/tmt_running"
+}
+
+
+
 
 if [[ "$TESTS" != "test" ]] && [[ "$TESTS" != "test-pytest" ]] && [[ "$TESTS" != "test-upstream" ]] && [[ "$TESTS" != "test-openshift-pytest" ]] && [[ "$TESTS" != "test-openshift-4" ]]; then
   echo "This test scenario is not enabled."
   exit 1
 fi
 
-SCRIPT="daily_scl_tests"
-WORK_DIR=$(mktemp -d -p "/var/tmp")
-git clone "$TMT_REPO" "$WORK_DIR/$TMT_DIR"
+
 CWD=$(pwd)
-cd $HOME || { echo "Could not switch to $HOME"; exit 1; }
-if [[ ! -d "${LOGS_DIR}" ]]; then
-  mkdir -p "${LOGS_DIR}"
-fi
-if [[ ! -d "${RESULTS_DIR}" ]]; then
-  mkdir -p "${RESULTS_DIR}"
-fi
-COMPOSE=$(tmt -q run provision -h minute --list-images | grep $COMPOSE | head -n 1 | tr -d '[:space:]')
-DIR="${DAILY_TEST_DIR}/${TARGET}-${TESTS}-${SET_TEST}"
-if [[ "$TESTS" == "test-upstream" ]]; then
-  DIR="${DAILY_TEST_DIR}/${TARGET}-${TESTS}"
-fi
-mkdir -p "${RESULTS_DIR}/${TARGET}-${TESTS}/plans/${TFT_PLAN}/data/results"
-mkdir -p "$DIR"
-LOG="${LOGS_DIR}/$TARGET-$TESTS.log"
+cd "$HOME" || { echo "Could not switch to $HOME"; exit 1; }
+prepare_environment
+get_compose
+
+move_logs_to_old
 
 date > "${LOG}"
-curl --insecure -L https://url.corp.redhat.com/fmf-data > /tmp/fmf_data
-source /tmp/fmf_data
+curl --insecure -L https://url.corp.redhat.com/fmf-data > "/tmp/fmf_data"
+source "/tmp/fmf_data"
 
 cd "$WORK_DIR/$TMT_DIR" || { echo "Could not switch to $WORK_DIR/$TMT_DIR"; exit 1; }
 echo "TARGET is: ${TARGET} and test is: ${TESTS}" | tee -a "${LOG}"
-RESULTS_TARGET_DIR="${RESULTS_DIR}/${TARGET}-${TESTS}"
-touch "${RESULTS_TARGET_DIR}/tmt_running"
-if [[ "$TESTS" == "test-upstream" ]]; then
-  TMT_COMMAND="tmt run -v -v -d -d --all -e DEBUG=yes -e SCRIPT=$SCRIPT -e OS=$TARGET -e TEST=$TESTS --id ${DIR} plan --name $TFT_PLAN provision --how minute --auto-select-network --image ${COMPOSE}"
-else
-  TMT_COMMAND="tmt run -v -v -d -d --all -e DEBUG=yes -e SCRIPT=$SCRIPT -e OS=$TARGET -e SET_TEST=$SET_TEST -e TEST=$TESTS --id ${DIR} plan --name $TFT_PLAN provision --how minute --auto-select-network --image ${COMPOSE}"
-fi
-echo "TMT command is: $TMT_COMMAND" | tee -a "${LOG}"
-set -o pipefail
-$TMT_COMMAND | tee -a "${LOG}"
-if [[ $? -ne 0 ]]; then
-  echo "TMT command $TMT_COMMAND has failed."
-  if [[ -f "${RESULTS_TARGET_DIR}/tmt_success" ]]; then
-    rm -f "${RESULTS_TARGET_DIR}/tmt_success"
-  fi
-  touch "${RESULTS_TARGET_DIR}/tmt_failed"
-else
-  if [[ -f "${RESULTS_TARGET_DIR}/tmt_failed" ]]; then
-    echo "Previous test run has failed but this one has passed."
-  else
-    touch "${RESULTS_TARGET_DIR}/tmt_success"
-  fi
-fi
-if [[ -d "${DIR}/plans/${TFT_PLAN}/data" ]]; then
-  cp -rv "${DIR}/plans/${TFT_PLAN}/data/results" "${RESULTS_TARGET_DIR}/plans/${TFT_PLAN}/data/"
-  cp -v "${DIR}/plans/${TFT_PLAN}/data/*.log" "${RESULTS_TARGET_DIR}/plans/${TFT_PLAN}/data/"
-fi
-cp "${DIR}/log.txt" "${RESULTS_TARGET_DIR}/"
-set +o pipefail
-rm -f "${RESULTS_TARGET_DIR}/tmt_running"
+
+run_tests
+
 cd "$CWD" || exit 1
 rm -rf "$WORK_DIR"
