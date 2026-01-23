6be5274d1731047cf431c74ee35e07e525307620
Petr "Stone" Hracek <phracek@redhat.com>
2026-01-23 12:53:28 +0100

    Move all cloned directory to owr own directory
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- requirements.sh
+++ requirements.sh
@@ -5,3 +5,5 @@ set -ex
 useradd -u 500 -r -g 0 -m -s /bin/bash -c "Default Application User" "${NAME}"
 chown -R 500:0 "${HOME}"
 chmod -R a+rwx "${HOME}"
+chmod -R a+rwx "${WORK_DIR}"
+chown -R 500:0 "${WORK_DIR}"
