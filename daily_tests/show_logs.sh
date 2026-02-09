a5280ab62e306295bdd350d43551609263665fb6
Petr "Stone" Hracek <phracek@redhat.com>
2026-02-09 14:40:53 +0100

    Use relative path instead of full_path
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- daily_tests/show_logs.sh
+++ daily_tests/show_logs.sh
@@ -2,10 +2,12 @@
 
 set -x
 
+cd /root/ci-scripts/daily_tests
+
 while true; do
     echo "Displaying log files every 10 minutes..."
     date
-    python3 /root/ci-scripts/show_logs.py
+    python3 ./show_logs.py
     # Let's sleep for 10 minutes
     sleep 600
 done
