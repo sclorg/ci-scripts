0e1db5d46d44a8af07605b97a275be000f795078
Petr "Stone" Hracek <phracek@redhat.com>
2026-02-23 14:12:47 +0100

    Fix calling report with parameter
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- daily_tests/daily_nightly_tests_report.sh
+++ daily_tests/daily_nightly_tests_report.sh
@@ -10,4 +10,8 @@ echo "Bash arguments: $@"
 env
 find "/var/ci-scripts/daily_reports_dir/${CUR_DATE}" -type f -name "tmt_*"
 echo "--------------------"
-python3 ./daily_nightly_tests_report.py "$1"
+if [ -n "$1" ]; then
+    python3 ./daily_nightly_tests_report.py "$1"
+else
+    python3 ./daily_nightly_tests_report.py
+fi
