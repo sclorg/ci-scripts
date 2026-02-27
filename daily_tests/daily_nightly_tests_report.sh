77bcf92f5a5b34e8240144ac924170f413099116
Petr "Stone" Hracek <phracek@redhat.com>
2026-02-27 13:13:53 +0100

    Add 10 seconds in case reporter send a big mail
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- daily_tests/daily_nightly_tests_report.sh
+++ daily_tests/daily_nightly_tests_report.sh
@@ -5,9 +5,6 @@ set -x
 cd /root/ci-scripts/daily_tests
 CUR_DATE=$(date +%Y-%m-%d)
 
-echo "Bash arguments: $@"
-# Let's sleep for 10 minutes
-env
 find "/var/ci-scripts/daily_reports_dir/${CUR_DATE}" -type f -name "tmt_*"
 echo "--------------------"
 if [ -n "$1" ]; then
@@ -15,3 +12,5 @@ if [ -n "$1" ]; then
 else
     python3 ./daily_nightly_tests_report.py
 fi
+# Sleep 10 seconds in case we need to send a bigger mail.
+sleep 10
