7543a3f439afa46d609006dd6bc6fa01c97e92da
Petr "Stone" Hracek <phracek@redhat.com>
2026-03-12 09:48:58 +0100

    Update .gitignore similar like in container-ci-suite
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- daily_tests/tests/integration/test_integration_placeholder.py
+++
@@ -1,9 +0,0 @@
-"""Placeholder for integration tests. Add real integration tests here."""
-
-import pytest
-
-
-@pytest.mark.integration
-def test_integration_placeholder():
-    """Placeholder test until real integration tests are added."""
-    pass
